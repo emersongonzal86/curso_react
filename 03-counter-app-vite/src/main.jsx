53b997f0f16b7f11fa971a96b773458e2bedbf83
emerson <emersongonzal86@gmail.com>
2022-12-18 12:28:11 -0500

    first application in React Hello word

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//Esta es mi aplicación
+// Los componentes usualmente no estan en el archvo main esto es solo 
+//para el ejemplo
+
+function App (){
+    return (<h1>Hola Mundo !!!!!</h1>);
+}
+
+//Aca vamos a renderizar la aplicación
+
+ReactDOM.createRoot( document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
