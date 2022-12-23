52ad412e0b517303e8d4ec687cd369f21ea4ef5d
emerson <emersongonzal86@gmail.com>
2022-12-23 17:52:30 -0500

    Event

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,15 +1,24 @@
+//Evento Click
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({value}) => {
 
-    //console.log(props);
+  const handleAdd = ( event ) =>{
+    console.log(event)
+  }
     
     return (
+
+      
+
       <>
       <h1>Counter App</h1>
          
           <p> { value }</p>
-          
+        
+        <button onClick={ handleAdd }>
+          +1
+        </button>
           
       </>
       
