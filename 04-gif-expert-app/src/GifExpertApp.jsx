da13c9eb8d0490f52d9ac593ca269282d71c46cf
emerson <emersongonzal86@gmail.com>
2023-01-05 17:14:15 -0500

    GifExpertApp-Component

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -2,8 +2,16 @@
 export const GifExpertApp = () => {
   return (
     <>
+
+    {/* Título */}
         <h1>GifExpertApp</h1>
 
+    {/*Input */}
+
+
+    {/** Listado de Gifs */}
+
+        {/* Gif Item */}
     </>
   )
 }
