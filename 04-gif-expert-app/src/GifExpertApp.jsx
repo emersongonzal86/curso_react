d9bec0258cd7a27958f16645af1ea0fbc8dfffe2
emerson <emersongonzal86@gmail.com>
2023-01-06 17:17:38 -0500

    Adding a new category

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,25 +1,22 @@
-// Creando lista de categorias
+// Agregando una nueva categoria
 import { useState } from 'react';
 export const GifExpertApp = () => {
-/* Para almacenar información y esa información tiene que cambiar el html lo hacenos con un Hook de React para
-mantener el estado
-usanos el useStateSnippet para crear el codigo const [first, setfirst] = useState(second) setCategories siempre uselo camelCase
-vamos a crear las categorias el nombre de una categoria es One Punch
-el usesState debe tener un valor inicial y debe ser importado
 
-LOS HOOKS EN REACT NO TIENEN NOMBRE ESTAN BASADOS EN SU POSICION INICIANDO EN 1
-los hooks no se pueden colocar dentro de condicionales deben trabajarse continuos para que no pierdan su posicion
-
-const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball']);
-const [categories2, setCategories2] = useState([ 'anime', 'manga']);
-const [categories3, setCategories3] = useState([ 'Musica', 'Naturaleza']);
-en las herramientas de desarrollador aparece una varita magica que sirve para parsear los nombres
-para darnos una idea del objeto que tenemos 
-*/  
 
   const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball']);
 
-   console.log(categories);
+  const onAddCategory =() => {
+    // los tres puntos antes de categories se usan para desestructurar el objeto
+    //de esta forma inserta una nueva categoria al final de la lista
+    setCategories([...categories,'Nueva Categoria']);
+
+    //de esta manera insterna la nueva categoria l inicio de la lista 
+    setCategories(['Nueva CAtegoria', ...categories])
+
+    //La siguiente es otra forma de hacerlo utilizando el elemento callback de setCategories
+    // setCategories(cat => [...cat, 'Nueva categoria']);
+
+  }
 
   return (
     <>
@@ -30,10 +27,10 @@ para darnos una idea del objeto que tenemos
     {/*Input */}
 
 
-    {/** Listado de Gifs renderizando listado d ecategorias*/}
-{/* esto es un error porque falta la propertyKey que es una llave de valor unico No importa lo que sea
-pero debe ser valor unico puedn ser fechas numeros en este ejemplo asumimos category como unica
-Warning: Each child in a list should have a unique "key" prop */}
+    {/** Listado de Gifs */}
+
+    <button onClick={ onAddCategory }> Agregar</button>
+
         <ol>
             { categories.map( category => {
               return <li key={ category }> { category } </li>
