0ef9d42f4b6315ab9b66f66d3be088b6028ea2c1
emerson <emersongonzal86@gmail.com>
2023-01-13 17:43:17 -0500

    show the titles of the desired images

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,31 +1,38 @@
-//useEffect
-/** El useEffect es un hook de react que se usa para disparar efectos secundarios entendidos 
- * como un proceso que se quiere ejecutar cuando algo suceda por ejemplo cuando el counte cambie quiero disparar un  efecto
- * cuando la categiria cambia quiero disparar un efecto
- * el useEffect tiene dos argumentos que son el efecto propiamente qu queremos dispararq ue es una funcion callback
- * es decir cualquier funcion y una lista de dependencias que son la lista de condiciones por las cuales
- * se quiere volver a ejecutar el callback las [] es donde se coloca el arreglo con las dependencias si se dejan vacias
- * significa que este hook se va a disparar la primera vez que se crea o construye el componente
- */
+//Mostrar los titulos de las imagenes
+
 import { useState, useEffect } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
 
-  const [counter, setCounter] = useState(10);
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  }
 
   useEffect(() => {
-    getGifs(category);
+    getImages();
   }, [])
 
-  
+
 
   return (
     <>
 
       <h3>{category}</h3>
-      <h5> { counter} </h5>
-      <button onClick={ () => setCounter( counter + 1 )}>+1</button>
+      <ol>
+        {
+          images.map(({ id, title })=>(
+            <li key={ id }>{ title }</li>
+          ))
+        }
+
+      </ol>
+
+
+
     </>
   )
 }
