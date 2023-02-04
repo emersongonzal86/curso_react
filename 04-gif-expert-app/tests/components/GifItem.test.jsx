1dc51f5bb4809142c176eb1980c3e9653f61127a
emerson <emersongonzal86@gmail.com>
2023-02-04 17:35:23 -0500

    test on the GifGridItem component

--- 04-gif-expert-app/tests/components/GifItem.test.jsx
+++ 04-gif-expert-app/tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifItem } from '../../src/components/GifItem';
 
 describe('Pruebas en <GifItem/>',() =>{
@@ -10,5 +10,23 @@ describe('Pruebas en <GifItem/>',() =>{
         const { container } = render(<GifItem title={ title } url={ url } />);
         expect( container ).toMatchSnapshot();
     });
+
+    test('Debe de mostrar la imagen con el URL y el ALT indicado', () => {
+        render(<GifItem title={ title } url={ url } />);
+        //importando screen de testing-library-react para ver como se esta renderizando el sujeto de prueba por la consola
+        //screen.debug();
+        //expect(screen.getByRole('img').src).toBe( url );
+        //expect(screen.getByRole('img').alt).toBe( alt );
+
+        const { setPointerCapture, alt} = screen.getByRole('img');
+        expect ( src ).toBe( url );
+        expect ( alt ).toBe( alt );
+    });
+
+    test('debe de mostrar el t[itulo ne le componente', () => {
+        render(<GifItem title={ title } url={ url } />);
+        expect( screen.getByText( title )).toBeTruthy();
+    })
+    
 
 });
