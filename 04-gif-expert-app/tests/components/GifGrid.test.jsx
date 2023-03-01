0c18536d3bc8cb27465b79910ef797728360a808
emerson <emersongonzal86@gmail.com>
2023-02-28 20:04:47 -0500

    make a full mock of a customhook

--- 04-gif-expert-app/tests/components/GifGrid.test.jsx
+++ 04-gif-expert-app/tests/components/GifGrid.test.jsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Pruebas en <GifGrid />', () => {
 
@@ -8,13 +10,42 @@ describe('Pruebas en <GifGrid />', () => {
 
     test('Debe de mostrar el loading inicialmente', () => {
 
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
+
         render(<GifGrid category={category} />);
         //screen.debug();
         expect(screen.getByText('Cargando.....'));
         expect(screen.getByText(category));
     });
 
-    test('debe de mostrar items cuando se cargan las imagenes useFetch', () => { 
+    test('debe de mostrar items cuando se cargan las imagenes useFetchGifs', () => {
+
+        const gifs = [
+            {
+                id:'ABC',
+                title:'Saitama',
+                url: 'https://loclhost/saitama.jpg'
+
+            },
+            {
+                id:'123',
+                title:'Goku',
+                url: 'https://loclhost/goku.jpg'
+
+            },
+        ]
         
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+
+
+        render(<GifGrid category={category} />);
+        expect( screen.getAllByRole( 'img' ).length ).toBe(2);
+
     });
 });
