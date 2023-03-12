3bfa5a2754545f6b436f8d175c822bcb479a3103
emerson <emersongonzal86@gmail.com>
2023-03-12 13:12:44 -0500

    Form with customHook

---
+++ 05-hook-app/src/hooks/useForm.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+export const useForm = ( initialForm = {} ) => {
+  
+    const [ formState, setFormState ] = useState( initialForm );
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            //desestructuro el formulario para traer todos los valores para mantenerlos
+            ...formState,
+            // voy a modificar la qu el name este siendo modificada para eso utilizo de javascript
+            //las propiedades computadas de los objetos colocando la propiedad entre [] llave cuadrada
+            [name]: value
+
+        });
+
+    }
+
+    return{
+        //desestructuro el formState para obtener todos sus valores y ahorra la desestructuracion en el FormWithCustomHook
+        // asi pasamos todos los valores del formulario para tenerlso dsiponibles en 
+        // const { formState, onInputChange, username, email, password } = useForm({
+        //     username: '',
+        //     email: '',
+        //     password: ''
+    
+        // });
+        ...formState,
+        formState,
+        onInputChange,
+
+    }
+}
+
+
