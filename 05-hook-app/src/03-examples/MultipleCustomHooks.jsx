3deabd1d59303b5b4fb3b69b0a208552317fc2ad
emerson <emersongonzal86@gmail.com>
2023-03-15 19:32:35 -0500

    code rendering

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,43 +1,32 @@
-import { useFetch } from '../hooks/useFetch';
-import{ useCounter } from '../hooks/useCounter';
+import { useCounter, useFetch } from '../hooks';
+import { LoadingQuote, Quote } from './';
 
 export const MultipleCustomHooks = () => {
-  const {counter, increment} = useCounter(1);
+  const { counter, increment } = useCounter(1);
 
-  const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${counter}` );
-  
-  const { author, quote } = !!data && data[0] ;
+  const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${counter}`);
 
-    return (
+  const { author, quote } = !!data && data[0];
+
+  return (
     <>
-      
-        <h1>BreakingBad Quotes</h1>
-        <hr />
 
-        {
-          isLoading
-              ?
-             (
-                <div className="alert alert-info text-center">
-                Loading....
-                </div>
-             )
-             :(
-                <blockquote className="blockquote text-end">
-                   <p className="mb-1"> { quote } </p>
-                   <footer className="blockquote-footer"> { author } </footer>
-                 </blockquote>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
 
-             )
-        }
+      {
+        isLoading
+          ? <LoadingQuote />
+          : <Quote author={author} quote={quote} />
+      }
 
-        <button className="btn btn-primary" 
-                disabled= {isLoading }
-                onClick={ () => increment() }>
-          Next quote
-        </button>
+      <button className="btn btn-primary"
+        disabled={isLoading}
+        onClick={() => increment()}>
+        Next quote
+      </button>
 
-        
+
     </>
   )
 }
