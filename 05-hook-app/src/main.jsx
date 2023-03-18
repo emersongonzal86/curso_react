419d2f39c2e41da08933e67a63fcebcf1df57e0b
emerson <emersongonzal86@gmail.com>
2023-03-18 17:54:33 -0500

    useCallback

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { MemoHook } from './06-memos/MemoHook';
+import { CallbackHook } from './06-memos/CallbackHook';
+// import { MemoHook } from './06-memos/MemoHook';
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
@@ -14,6 +15,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MemoHook />
+    <CallbackHook />
   </React.StrictMode>,
 )
