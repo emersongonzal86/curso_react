31050a515bb38d711ccdc116c5fdedb80c9ee2f5
emerson <emersongonzal86@gmail.com>
2023-03-27 18:58:50 -0500

    Lista de TODOS fallando

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,17 +1,16 @@
 import { useReducer } from 'react';
+import { TodoAdd } from './TodoAdd';
+import { TodoList } from './TodoList';
 import { todoReducer } from './todoReducer';
 
 const initialState = [
     {
-        // new Date().getTime() se utiliza para obtener la fecha actual pero solo los numeros crea un id unico
-        //ocasionalmente pued generar duplicado si se hace en elmismo milisegundo pero por eso en este ejemplo lo multiplican po 3 
         id: new Date().getTime(),
         descrption: 'Recolectar la piedra del alma',
         done: false,
 
     },
     {
-
         id: new Date().getTime() * 3,
         descrption: 'Recolectar la piedra del poder',
         done: false,
@@ -25,6 +24,15 @@ export const TodoApp = () => {
 
     const [todos, dispatch] = useReducer(todoReducer, initialState)
 
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            pauload: todo
+        }
+
+        dispatch( action );
+    }
+
     return (
         <>
             <h1>TodoApp: 10 <small>Pendientes: 2</small></h1>
@@ -32,42 +40,14 @@ export const TodoApp = () => {
 
             <div className="row">
                 <div className="col-7">
-                    <ul className="list-group">
-                        {
-                            todos.map( todo => (
-                                    <li className="list-group-item d-flex justify-content-between">
-                            <span className="align-self-center">Item 1</span>
-                            <button className="btn btn-danger">Borrar</button>
-                        </li>
-
-                            ))
-                        }
- 
-                    </ul>
-
+                    <TodoList todos={ todos } />
                 </div>
 
-                <div clasName="col-5">
+                <div className="col-5">
 
                     <h4>Agregar TODO</h4>
                     <hr />
-                    <form>
-                        <input
-                            type="text"
-                            placeholder="Que hay que hace?"
-                            className="form-control"
-
-                        />
-                        <button
-                            type="submit"
-                            className="btn btn-outline-primary mt-1"
-
-                        >
-                            Agregar
-                        </button>
-
-
-                    </form>
+                    <TodoAdd onNewTodo={ handleNewTodo }/>
 
                 </div>
 
--- 05-hook-app/src/08-useReducer/todoReducer.js
+++ 05-hook-app/src/08-useReducer/todoReducer.js
@@ -1,12 +1,12 @@
 
 
-export const todoReducer = ( initialState, action ) =>{
+export const todoReducer = ( initialState =[], action ) =>{
 
 
     switch ( action.type ) {
-        case 'ABC':
-            //en caso de no tener algo implementado se coloca throw new Error( 'ction.type = ABC no está implementada');
-            return initialState;
+        case '[TODO] Add Todo':
+            //en caso de no tener algo implementado se coloca throw new Error( 'Action.type = ABC no está implementada');
+            return [ ...initialState, action.payload ]
     
         default:
             return initialState;
