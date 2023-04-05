618271dbd27a329439c2ca6e01a6175bc8ec228d
emerson <emersongonzal86@gmail.com>
2023-04-04 20:10:09 -0500

    NavLink

--- 05-hook-app/src/09-useContext/Navbar.jsx
+++ 05-hook-app/src/09-useContext/Navbar.jsx
@@ -1,11 +1,36 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 export const Navbar = () => {
   return (
-    <>
-     <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/login">Login</Link> 
-    </>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary roundes-3">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">useContext</Link>
+        
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            
+            <NavLink 
+                className={( { isActive }) => `nav-link ${ isActive ? 'active' : ''}`}
+                to="/" >
+              Home
+            </NavLink>
+
+            <NavLink 
+                className={( { isActive }) => `nav-link ${ isActive ? 'active' : ''}`}
+                to="/about" >
+              About
+            </NavLink>
+
+            <NavLink 
+                className={( { isActive }) => `nav-link ${ isActive ? 'active' : ''}`}
+                to="/Login" >
+              Login
+            </NavLink>
+           
+            
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
